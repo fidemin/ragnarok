4bbf6258d5e791d0c8e655211a92c359c509c183
Yunhong Min <yunhongmin00@gmail.com>
2023-09-18 20:47:37 +0900

    gradient function for one layer

--- core/layer.py
+++ core/layer.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from core import numerical
+
 
 class Layer:
     def __init__(self, input_size, output_size, weight_init=0.01):
@@ -13,7 +15,13 @@ class Layer:
 
         self.W = weight_init * np.random.randn(input_size, output_size)
         self.b = np.zeros(output_size)
+        self.W_grad = np.zeros_like(self.W)
+        self.b_grad = np.zeros_like(self.b)
 
     def predict(self, x, activation_func):
         a = np.dot(x, self.W) + self.b
         return activation_func(a)
+
+    def gradient(self, loss_func):
+        self.W_grad = numerical.gradient(loss_func, self.W)
+        self.b_grad = numerical.gradient(loss_func, self.b)
--- core/test_layer.py
+++ core/test_layer.py
@@ -1,10 +1,27 @@
 import numpy as np
 
 from core.activation import sigmoid
-from .layer import Layer
+from core.layer import Layer
+
+
+def func1(x: np.ndarray) -> np.ndarray:
+    return np.power(x, 2)
 
 
 def test_predict():
     layer = Layer(2, 3)
     predict = layer.predict(np.array([1.0, 2.0]), sigmoid)
     assert predict.shape[0] == 3
+
+
+def test_gradient():
+    layer = Layer(2, 3)
+    layer.W = np.array([[3.0, 4.0], [0.0, 2.0], [5.0, 0.0]])
+    layer.b = np.array([1.0, 2.0, 0.0])
+
+    expected_W_grad = np.array([[6.0, 8.0], [0.0, 4.0], [10.0, 0.0]])
+    expected_b_grad = np.array([2.0, 4.0, 0.0])
+
+    layer.gradient(func1)
+    assert np.allclose(layer.W_grad, expected_W_grad)
+    assert np.allclose(layer.b_grad, expected_b_grad)
