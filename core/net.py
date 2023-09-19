fix gradient descent calculation

core/net.py
@@ -50,11 +50,16 @@ class Net:
         for i in range(len(self.layers)):
             self.layers[i].gradient(loss_func)
 
-    def gradient_descent(self, x: np.ndarray, t: np.ndarray, lr=0.01, n_iter=100):
+    def gradient_descent(self, x: np.ndarray, t: np.ndarray, lr=0.01):
         loss_func = lambda W: self.loss(x, t)
 
+        # first calculate gradient for each layer with CURRENT W, b of layers.
         for i in range(len(self.layers)):
-            self.layers[i].gradient_descent(loss_func, lr=lr, n_iter=n_iter)
+            self.layers[i].gradient(loss_func)
+
+        # second update parameter W, b in layers.
+        for i in range(len(self.layers)):
+            self.layers[i].update_params_from_gradient_descent(lr)
 
 
 class NetInitException(Exception):

core/test_layer.py
@@ -27,12 +27,16 @@ def test_gradient():
     assert np.allclose(layer.b_grad, expected_b_grad)
 
 
-def test_gradient_descent():
+def test_update_params_from_gradient_descent():
     layer = Layer(2, 3)
     layer.W = np.array([[3.0, 4.0, 2.0], [0.0, 2.0, 5.0]])
     layer.b = np.array([1.0, 2.0, 0.0])
 
-    layer.gradient_descent(func1, lr=0.1, n_iter=1)
+    layer.W_grad = np.array([[6.0, 8.0, 4.0], [0.0, 4.0, 10.0]])
+    layer.b_grad = np.array([2.0, 4.0, 0.0])
+
+    layer.gradient(loss_func=func1)
+    layer.update_params_from_gradient_descent(lr=0.1)
 
     expected_W = [[2.4, 3.2, 1.6], [0.0, 1.6, 4.0]]
     expected_b = [0.8, 1.6, 0.0]

examples/two_layer_net.py
@@ -1,6 +1,8 @@
 import numpy as np
 from keras.datasets import mnist
 
+from core import net
+
 
 def convert_to_one_hot_encoding(y: np.ndarray):
     result = np.zeros((y.size, np.max(y) + 1))
@@ -9,16 +11,35 @@ def convert_to_one_hot_encoding(y: np.ndarray):
 
 
 if __name__ == '__main__':
-    (train_X, train_y_origin), (test_X, test_y_origin) = mnist.load_data()
+    (train_X_origin, train_y_origin), (test_X_origin, test_y_origin) = mnist.load_data()
+    train_X = train_X_origin.reshape(train_X_origin.shape[0], train_X_origin.shape[1] * train_X_origin.shape[2])
     train_y = convert_to_one_hot_encoding(train_y_origin)
+    test_X = test_X_origin.reshape(test_X_origin.shape[0], test_X_origin.shape[1] * test_X_origin.shape[2])
     test_y = convert_to_one_hot_encoding(test_y_origin)
 
-    train_loss_list = []
+    loss_list = []
 
-    iter_num = 10000
-    train_size = train_X.size
+    # iter_num = 10000
+    iter_num = 10
+    train_size = train_X.shape[0]
     batch_size = 100
     learning_rate = 0.1
 
-    print(train_y[:10])
-    print(test_y[:10])
+    input_size = train_X.shape[1]
+    output_size = train_y.shape[1]
+    hidden_size = 50
+
+    two_layer_net = net.Net([input_size, hidden_size, output_size])
+
+    for i in range(iter_num):
+        print("iter_num: {} starts".format(i))
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = train_X[batch_mask]
+        y_batch = train_y[batch_mask]
+
+        two_layer_net.gradient_descent(x_batch, y_batch, lr=0.1)
+
+        loss = two_layer_net.loss(x_batch, y_batch)
+        loss_list.append(loss)
+
+    print(loss_list)