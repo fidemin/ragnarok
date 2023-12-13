[bugfix] variable backward gradient should be Variable

ragnarok/core/test_variable.py
@@ -35,7 +35,6 @@ class TestVariable:
             Variable(test_input)
 
     def test_backward(self):
-        # TODO: test_backward for Add function
         test_input = Variable(np.array([0.1, 0.2]))
 
         f1 = Square()
@@ -99,6 +98,15 @@ class TestVariable:
 
         expected = numerical_diff(complex_function, test_input)
 
+        assert allclose(test_input.grad, expected)
+
+    def test_backward_with_same_inputs(self):
+        test_input = Variable(np.array([0.1, 0.2, 0.3]))
+        f = Add()
+        output = f(test_input, test_input)
+        output.backward()
+
+        expected = Variable(np.array([2.0, 2.0, 2.0]))
         assert allclose(test_input.grad, expected)
 
     def test_set_creator(self):

ragnarok/core/variable.py
@@ -45,7 +45,7 @@ class Variable:
             raise VariableError('The creator of this variable is None. backward propagation is not possible.')
 
         if self._grad is None:
-            self._grad = np.ones_like(self.data)
+            self._grad = Variable(np.ones_like(self.data))
 
         # initial value
         functions = [self._creator]
@@ -73,7 +73,11 @@ class Variable:
             inputs = function.inputs
 
             for input_, dinput in zip(inputs, dinputs):
-                input_.grad = dinput
+                if input_.grad is not None:
+                    input_.grad = Variable(input_.grad.data + dinput.data)
+                else:
+                    input_.grad = dinput
+
                 if input_.creator is not None:
                     next_creator = input_.creator
                     functions.append(next_creator)