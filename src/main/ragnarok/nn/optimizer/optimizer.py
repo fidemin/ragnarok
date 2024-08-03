4269f23ffb1fe9af4e61105ddd0b2f2cd6815d79
Yunhong Min <yunhongmin00@gmail.com>
2024-08-03 12:35:33 +0900

    add adam optimizer

--- src/main/ragnarok/nn/optimizer/optimizer.py
+++ src/main/ragnarok/nn/optimizer/optimizer.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
-from typing import Iterable, Optional
+from typing import Iterable, Optional, List
+
+import numpy as np
 
 from src.main.ragnarok.core.variable.variable import zeros_like, Variable
 from src.main.ragnarok.nn.core.parameter import Parameter
@@ -8,11 +10,11 @@ from src.main.ragnarok.nn.core.parameter import Parameter
 class Optimizer(metaclass=ABCMeta):
     def update(self, params: Iterable[Parameter]):
         self._pre_update(params)
-        for param in params:
-            self._update_one(param)
+        for idx, param in enumerate(params):
+            self._update_one(idx, param)
 
     @abstractmethod
-    def _update_one(self, param: Parameter):
+    def _update_one(self, idx: int, param: Parameter):
         # Implementation required
         pass
 
@@ -24,14 +26,14 @@ class SGD(Optimizer):
     def __init__(self, lr=0.01):
         self.lr = lr
 
-    def _update_one(self, param: Parameter):
+    def _update_one(self, idx: int, param: Parameter):
         # NOTE: need to update referenced value
         # NOTE: need to use numpy operation(low level) to avoid creating graph
         param.data -= self.lr * param.grad.data
 
 
 class Momentum(Optimizer):
-    _v: Optional[dict[int, Variable]]
+    _v: Optional[List[Variable]]
 
     def __init__(self, lr=0.01, momentum=0.9):
         self._lr = lr
@@ -40,11 +42,46 @@ class Momentum(Optimizer):
 
     def _pre_update(self, params: Iterable[Parameter]):
         if self._v is None:
-            self._v = {}
+            self._v = []
             for param in params:
-                self._v[id(param)] = zeros_like(param)
+                self._v.append(zeros_like(param))
 
-    def _update_one(self, param: Parameter):
-        v = self._v[id(param)]
+    def _update_one(self, idx: int, param: Parameter):
+        v = self._v[idx]
         v.data = self._momentum * v.data - self._lr * param.grad.data
         param.data += v.data
+
+
+class Adam(Optimizer):
+    def __init__(self, lr=0.001, beta1=0.9, beta2=0.999):
+        self._lr = lr
+        self._beta1 = beta1
+        self._beta2 = beta2
+        self._iter = 0
+        self._momentum1 = []
+        self._momentum2 = []
+        self._epsilon = 1e-8
+
+    def _pre_update(self, params: Iterable[Parameter]):
+        if self._iter == 0:
+            for param in params:
+                self._momentum1.append(zeros_like(param))
+                self._momentum2.append(zeros_like(param))
+
+        self._iter += 1
+
+    def _update_one(self, idx: int, param: Parameter):
+        self._momentum1[idx].data = (
+            self._beta1 * self._momentum1[idx].data
+            + (1 - self._beta1) * param.grad.data
+        )
+        momentum1_corr = self._momentum1[idx].data / (1 - (self._beta1**self._iter))
+
+        self._momentum2[idx].data = self._beta2 * self._momentum2[idx].data + (
+            1 - self._beta2
+        ) * np.power(param.grad.data, 2)
+        momentum2_corr = self._momentum2[idx].data / (1 - (self._beta2**self._iter))
+
+        param.data -= (
+            self._lr * momentum1_corr / (np.sqrt(momentum2_corr) + self._epsilon)
+        )
--- src/test/ragnarok/optimizer/test_optimizer.py
+++ src/test/ragnarok/optimizer/test_optimizer.py
@@ -1,7 +1,8 @@
+from src.main.core.optimizer import Adam as AdamOld
 from src.main.ragnarok.core.util import allclose
 from src.main.ragnarok.core.variable import Variable
 from src.main.ragnarok.nn.core.parameter import Parameter
-from src.main.ragnarok.nn.optimizer.optimizer import SGD, Momentum
+from src.main.ragnarok.nn.optimizer.optimizer import SGD, Momentum, Adam
 
 
 class TestSGD:
@@ -44,3 +45,41 @@ class TestMomentum:
         # Then: Second update
         assert allclose(param1, Parameter([0.971, 1.942, 2.913]))
         assert allclose(param2, Parameter([3.884, 4.855, 5.826]))
+
+
+class TestAdam:
+    def test_update(self):
+        # Given
+        param1 = Parameter([1.0, 2.0, 3.0])
+        param2 = Parameter([4.0, 5.0, 6.0])
+        param1.grad = Variable([1.0, 2.0, 3.0])
+        param2.grad = Variable([4.0, 5.0, 6.0])
+
+        # When: First update
+        optimizer = Adam(lr=0.01, beta1=0.9, beta2=0.999)
+        optimizer.update([param1, param2])
+
+        old_optimizer = AdamOld(lr=0.01, beta1=0.9, beta2=0.999)
+        param1_old = Parameter([1.0, 2.0, 3.0])
+        param2_old = Parameter([4.0, 5.0, 6.0])
+        param1_old.grad = Variable([1.0, 2.0, 3.0])
+        param2_old.grad = Variable([4.0, 5.0, 6.0])
+        old_optimizer.optimize(
+            [param1_old.data, param2_old.data],
+            [param1_old.grad.data, param2_old.grad.data],
+        )
+
+        # Then: First update
+        assert allclose(param1, Parameter([0.99, 1.99, 2.99]))
+        assert allclose(param2, Parameter([3.99, 4.99, 5.99]))
+
+        # When: Second update
+        optimizer.update([param1, param2])
+        old_optimizer.optimize(
+            [param1_old.data, param2_old.data],
+            [param1_old.grad.data, param2_old.grad.data],
+        )
+
+        # Then: Second update
+        assert allclose(param1, Parameter([0.98, 1.98, 2.98]))
+        assert allclose(param2, Parameter([3.98, 4.98, 5.98]))
