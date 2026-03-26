f9d9276c4889f536a10baacec05b7803d5aa3a20
Yunhong Min <yunhongmin00@gmail.com>
2026-03-26 20:06:17 +0900

    feat: save/load module

--- ragnarok/nn/core/module.py
+++ ragnarok/nn/core/module.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod, ABCMeta
-from typing import List, Union
+from typing import List, Union, Dict
+
+import numpy as np
 
 from ragnarok.core.tensor import Tensor
 from ragnarok.nn.core.parameter import Parameter
@@ -26,24 +28,16 @@ class Module(metaclass=ABCMeta):
         self._set_init(key, value)
         super().__setattr__(key, value)
 
-    def _set_init(self, key, value):
-        # only register Parameter and Module to the container, other attributes are not registered
-        if isinstance(value, Parameter) or isinstance(value, Module):
-            key_str = f"{key}"
-            self._container[key_str] = value
-
     def __call__(self, *tensors: Tensor, **kwargs) -> Tensor | List[Tensor]:
         return self.forward(*tensors, **kwargs)
 
     @property
     def params(self) -> List[Parameter]:
-        params = []
-        for module_or_param in self._container.values():
-            if isinstance(module_or_param, Parameter):
-                params.append(module_or_param)
-            elif isinstance(module_or_param, Module):
-                params.extend(module_or_param.params)
-        return params
+        return list(self._params_dict().values())
+
+    @abstractmethod
+    def forward(self, *tensors: Tensor, **kwargs) -> Tensor | List[Tensor]:
+        pass
 
     def zero_grad(self):
         for module_or_param in self._container.values():
@@ -52,6 +46,54 @@ class Module(metaclass=ABCMeta):
             elif isinstance(module_or_param, Module):
                 module_or_param.zero_grad()
 
-    @abstractmethod
-    def forward(self, *tensors: Tensor, **kwargs) -> Tensor | List[Tensor]:
-        pass
+    def save(self, path: str):
+        params_dict = self._params_dict()
+
+        np_dict = {}
+        for param_name, param in params_dict.items():
+            np_dict[param_name] = param.data
+
+        np.savez_compressed(path, **np_dict, allow_pickle=False)
+
+    def load(self, path: str):
+        params_dict = self._params_dict()
+
+        np_dict = {}
+        for param_name, param in params_dict.items():
+            np_dict[param_name] = param.data
+
+        np.load(path, **np_dict)
+
+        for param_name, np_array in params_dict.items():
+            params_dict[param_name] = np_array
+
+    def _load_one_param(self, param_name: str, param: Parameter):
+        param_split = param_name.split("/")
+
+        if len(param_split) == 1:
+            self._container[param_split[0]] = param
+            return
+
+        # CASE: len(param_split) > 1
+        module_name = param_split[0]
+        self._container[module_name]._load_one_param("/".join(param_split[1:]), param)
+
+    def _params_dict(self) -> Dict[str, Parameter]:
+        params_dict = {}
+        for name, module_or_param in self._container.items():
+            if isinstance(module_or_param, Parameter):
+                params_dict[name] = module_or_param
+            elif isinstance(module_or_param, Module):
+                for sub_name, sub_param in module_or_param._params_dict().items():
+                    params_dict[name + "/" + sub_name] = sub_param
+            else:
+                raise ValueError(
+                    f"{type(module_or_param)} is not supported type for module container"
+                )
+        return params_dict
+
+    def _set_init(self, key, value):
+        # only register Parameter and Module to the container, other attributes are not registered
+        if isinstance(value, Parameter) or isinstance(value, Module):
+            key_str = f"{key}"
+            self._container[key_str] = value
--- tests/nn/core/test_module.py
+++ tests/nn/core/test_module.py
@@ -1,49 +1,166 @@
 from typing import List
 
+import pytest
+
 from ragnarok.core.tensor import Tensor
+from ragnarok.core.util import allclose
 from ragnarok.nn.core.module import Module
 from ragnarok.nn.core.parameter import Parameter
 
 
+@pytest.fixture
+def param_only_module_class():
+    class Layer(Module):
+        def __init__(self):
+            super().__init__()
+            self.weight = Parameter([0.1, 0.1, 0.1])
+            self.bias = Parameter([0.01, 0.02, 0.03])
+
+        def forward(self, *tensors: Tensor, **kwargs) -> Tensor | List[Tensor]:
+            tensor = tensors[0]
+            return tensor * self.weight + self.bias
+
+    return Layer
+
+
+@pytest.fixture
+def layer_only_module_class():
+    class Layer(Module):
+        def __init__(self):
+            super().__init__()
+            self.weight = Parameter([0.1, 0.1, 0.1])
+            self.bias = Parameter([0.01, 0.02, 0.03])
+
+        def forward(self, *tensors: Tensor, **kwargs) -> Tensor | List[Tensor]:
+            tensor = tensors[0]
+            return tensor * self.weight + self.bias
+
+    class Model(Module):
+        def __init__(self):
+            super().__init__()
+            self.layer = Layer()
+
+        def forward(self, *tensors: Tensor, **kwargs) -> Tensor | List[Tensor]:
+            tensor = tensors[0]
+            return self.layer(tensor)
+
+    return Model
+
+
+@pytest.fixture
+def complex_module_class():
+    class Layer(Module):
+        def __init__(self):
+            super().__init__()
+            self.weight = Parameter([0.1, 0.1, 0.1])
+            self.bias = Parameter([0.01, 0.02, 0.03])
+
+        def forward(self, *tensors: Tensor, **kwargs) -> Tensor | List[Tensor]:
+            tensor = tensors[0]
+            return tensor * self.weight + self.bias
+
+    class Model(Module):
+        def __init__(self):
+            super().__init__()
+            self.layer = Layer()
+            self.weight2 = Parameter([0.1])
+            self.bias2 = Parameter([0.0])
+
+        def forward(self, *tensors: Tensor, **kwargs) -> Tensor | List[Tensor]:
+            tensor = tensors[0]
+            y = self.layer(tensor)
+            y = y * self.weight2 + self.bias2
+            return y
+
+    return Model
+
+
 class TestModule:
-    def test_module_params_only(self):
-        from ragnarok.nn.core.module import Module
+    def test_init_params_only(self, param_only_module_class):
+        module = param_only_module_class()
+        assert module._container["weight"] is module.weight
+        assert module._container["bias"] is module.bias
+        assert module.params == [
+            module.weight,
+            module.bias,
+        ]
+        assert module._params_dict() == {
+            "weight": module.weight,
+            "bias": module.bias,
+        }
 
+    def test_init_layer_only(self, layer_only_module_class):
+        module = layer_only_module_class()
+        assert module._container["layer"] is module.layer
+        assert module.params == [
+            module.layer.weight,
+            module.layer.bias,
+        ]
+        assert module._params_dict() == {
+            "layer/weight": module.layer.weight,
+            "layer/bias": module.layer.bias,
+        }
+
+    def test_init_with_layer_and_param(self, complex_module_class):
+        module = complex_module_class()
+        assert module._container["layer"] is module.layer
+        assert module._container["weight2"] is module.weight2
+        assert module._container["bias2"] is module.bias2
+        assert module.params == [
+            module.layer.weight,
+            module.layer.bias,
+            module.weight2,
+            module.bias2,
+        ]
+        assert module._params_dict() == {
+            "layer/weight": module.layer.weight,
+            "layer/bias": module.layer.bias,
+            "weight2": module.weight2,
+            "bias2": module.bias2,
+        }
+
+    def test_save_and_load(self):
         class Layer(Module):
-            def __init__(self):
+            def __init__(self, multiplier: float):
                 super().__init__()
-                self.weight = Parameter([0.1, 0.1, 0.1])
-                self.bias = Parameter([0.01, 0.02, 0.03])
-
-            def forward(self, *tensors: Tensor, **kwargs) -> Tensor | List[Tensor]:
-                tensor = tensors[0]
-                return tensor * self.weight + self.bias
-
-        layer = Layer()
-        assert layer._container["weight"] is layer.weight
-        assert layer._container["bias"] is layer.bias
-        assert layer.params == [layer.weight, layer.bias]
-
-    def test_module_layer_only(self):
-        class Layer(Module):
-            def __init__(self):
-                super().__init__()
-                self.weight = Parameter([0.1, 0.1, 0.1])
-                self.bias = Parameter([0.01, 0.02, 0.03])
+                self.weight = multiplier * Parameter([0.1, 0.1, 0.1])
+                self.bias = multiplier * Parameter([0.01, 0.02, 0.03])
 
             def forward(self, *tensors: Tensor, **kwargs) -> Tensor | List[Tensor]:
                 tensor = tensors[0]
                 return tensor * self.weight + self.bias
 
         class Model(Module):
-            def __init__(self):
+            def __init__(self, layer: Layer, multiplier: float):
                 super().__init__()
-                self.layer = Layer()
+                self.layer = layer
+                self.weight2 = Parameter([0.1]) * multiplier
+                self.bias2 = Parameter([0.2]) * multiplier
 
             def forward(self, *tensors: Tensor, **kwargs) -> Tensor | List[Tensor]:
                 tensor = tensors[0]
-                return self.layer(tensor)
+                y = self.layer(tensor)
+                y = y * self.weight2 + self.bias2
+                return y
 
-        model = Model()
-        assert model._container["layer"] is model.layer
-        assert model.params == [model.layer.weight, model.layer.bias]
+        layer1 = Layer(2.0)
+        module_for_save = Model(layer1, 2.0)
+
+        tensor = Tensor([[0.1, 0.2, 0.3]])
+        expected = module_for_save(tensor)
+
+        file_name = "temp_test_module_1.npz"
+        module_for_save.save(file_name)
+
+        layer2 = Layer(1.5)
+        module_for_load = Model(layer2, 1.5)
+
+        actual = module_for_load(tensor)
+
+        # before load, result should be different
+        assert not allclose(expected, actual)
+
+        # after load, result should be same
+        module_for_load.load(file_name)
+        actual = module_for_load(tensor)
+        assert not allclose(expected, actual)
